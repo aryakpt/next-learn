a51132b67c571edbd866192b2a550b68036beb7f
aryakpt <aryakrisna07@gmail.com>
2023-03-28 14:52:39 +0700

    update timezone

--- src/services/worldtimeapi.ts
+++ src/services/worldtimeapi.ts
@@ -1,6 +1,6 @@
 export const apiGetWorldTime = async () => {
   try {
-    const res = await fetch('http://worldtimeapi.org/api/ip');
+    const res = await fetch('http://worldtimeapi.org/api/Asia/Jakarta');
     const response = await res.json();
     return response;
   } catch (error: any) {
